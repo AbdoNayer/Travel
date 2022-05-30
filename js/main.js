6005e05bf5bc6bc9d71d38aa1c931dc3c39182ac
AbdoNayer <abdelwahabnayer20@gmail.com>
2022-05-30 19:53:36 +0200

    done

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Window Load screen
+
+$(window).on('load', function () {
+
+    // Lazy Load
+    var lazyloadImages = document.querySelectorAll("img.lazy");    
+    var lazyloadThrottleTimeout;
+    
+    function lazyload () {
+      if(lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+      }    
+      
+      lazyloadThrottleTimeout = setTimeout(function() {
+          var scrollTop = window.pageYOffset;
+          lazyloadImages.forEach(function(img) {
+              if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                img.src = img.dataset.src;
+                img.classList.remove('lazy');
+              }
+          });
+          if(lazyloadImages.length == 0) { 
+            document.removeEventListener("scroll", lazyload);
+            window.removeEventListener("resize", lazyload);
+            window.removeEventListener("orientationChange", lazyload);
+          }
+      }, 20);
+    }
+    
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+
+    // Loading Page
+    $(".loader").fadeOut(500,function(){
+        $(".loading").delay(1000).fadeOut(500);
+    });
+    $("body").css("overflow-y", "auto");
+
+    // Animation Page
+    AOS.init();
+
+});
